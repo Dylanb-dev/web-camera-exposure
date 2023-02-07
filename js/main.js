b8f246e7dcbe90d7378b106cd1b97ac0b5ff29f0
Dylan <292206H@curtin.edu.au>
2023-02-07 13:18:32 +0800

    try again

--- js/main.js
+++ js/main.js
@@ -14,10 +14,10 @@ let count = 0
 // Put variables in global scope to make them available to the browser console.
 const constraints = window.constraints = {
     audio: false,
-    video: true,
+    video: { facingMode: "environment"},
     height: 320,
     width: 320,
-    facingMode: "environment"
+   
 };
 
 function handleSuccess(stream) {
